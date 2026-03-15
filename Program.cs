ec60bafb96fbfdadd4c1c1c37d7301d727cc893e
lucascano98 <lucascano98@outlook.com>
2026-03-15 20:34:26 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿public class ParkingSystem
+{
+    public int Big;
+    public int Medium;
+    public int Small;
+
+    public ParkingSystem(int big, int med, int small)
+    {
+        Big = big;
+        Medium = med;
+        Small = small;
+    }
+
+    public bool addCar(int carType)
+    {
+        switch(carType) {
+            case 1:
+                if (Big <= 0)
+                    return false;
+                else
+                {
+                    Big--;
+                    return true;
+                }
+            case 2:
+                if (Medium <= 0)
+                    return false;
+                else
+                {
+                    Medium--;
+                    return true;
+                }
+            case 3:
+                if (Small <= 0)
+                    return false;
+                else
+                {
+                    Small--;
+                    return true;
+                }
+            default:
+                return false;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ParkingSystem parkingSystem = new ParkingSystem(1, 1, 0);
+        Console.WriteLine(parkingSystem.addCar(1));
+        Console.WriteLine(parkingSystem.addCar(2));
+        Console.WriteLine(parkingSystem.addCar(3));
+        Console.WriteLine(parkingSystem.addCar(1));
+    }
+}
